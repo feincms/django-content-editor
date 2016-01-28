8d1552abb04873cfcf86fa4aade7241a08c85578
Matthias Kestenholz <mk@feinheit.ch>
2016-01-28 11:00:30 +0100

    changeform_view override not required

--- feincms2_content/admin.py
+++ feincms2_content/admin.py
@@ -50,7 +50,7 @@ class ItemEditor(ModelAdmin):
     """
     The ``ItemEditor`` is a drop-in replacement for ``ModelAdmin`` with the
     speciality of knowing how to work with :class:`feincms.models.Base`
-    subclasses and associated content types.
+    subclasses and associated plugins.
 
     It does not have any public API except from everything inherited from'
     the standard ``ModelAdmin`` class.
@@ -84,33 +84,6 @@ class ItemEditor(ModelAdmin):
             'regionTitles': [r.title for r in instance.template.regions],
         })
 
-    def changeform_view(self, request, object_id=None, form_url='',
-                        extra_context=None):
-        extra_context = extra_context or {}
-
-        if not object_id:
-            # insert dummy object as 'original' so template code can grab
-            # defaults for template, etc.
-            extra_context['original'] = self.model()
-
-            # If there are errors in the form, we need to preserve the object's
-            # template as it was set when the user attempted to save it, so
-            # that the same regions appear on screen.
-            if request.method == 'POST' and \
-                    hasattr(self.model, '_feincms_templates'):
-                extra_context['original'].template_key =\
-                    request.POST['template_key']
-
-        extra_context.update({
-            'request': request,
-            'model': self.model,
-            'available_templates': getattr(
-                self.model, '_feincms_templates', ()),
-            'FEINCMS_CONTENT_FIELDSET_NAME': FEINCMS_CONTENT_FIELDSET_NAME,
-        })
-        return super(ItemEditor, self).changeform_view(
-            request, object_id, form_url, extra_context)
-
     def render_change_form(self, request, context, **kwargs):
         if kwargs.get('add'):
             if request.method == 'GET' and 'adminform' in context:
@@ -120,12 +93,31 @@ class ItemEditor(ModelAdmin):
                 # ensure that initially-selected template in form is also
                 # used to render the initial regions in the item editor
 
-        context['item_editor_context'] = self._item_editor_context(
-            request,
-            context.get('original'),
-        )
-        return super(
-            ItemEditor, self).render_change_form(request, context, **kwargs)
+        # insert dummy object as 'original' if no original set yet so
+        # template code can grabdefaults for template, etc.
+        context.setdefault('original', self.model())
+
+        # If there are errors in the form, we need to preserve the object's
+        # template as it was set when the user attempted to save it, so
+        # that the same regions appear on screen.
+        if request.method == 'POST' and \
+                hasattr(self.model, '_feincms_templates'):
+            context['original'].template_key =\
+                request.POST['template_key']
+
+        context.update({
+            'request': request,
+            'model': self.model,
+            'available_templates': getattr(
+                self.model, '_feincms_templates', ()),
+            'FEINCMS_CONTENT_FIELDSET_NAME': FEINCMS_CONTENT_FIELDSET_NAME,
+            'item_editor_context': self._item_editor_context(
+                request,
+                context.get('original'),
+            ),
+        })
+        return super(ItemEditor, self).render_change_form(
+            request, context, **kwargs)
 
     @property
     def change_form_template(self):
@@ -157,11 +149,10 @@ class ItemEditor(ModelAdmin):
     # These next are only used if later we use a subclass of this class
     # which also inherits from VersionAdmin.
     revision_form_template = "admin/feincms/revision_form.html"
-
     recover_form_template = "admin/feincms/recover_form.html"
 
     def render_revision_form(self, request, obj, version, context,
                              revert=False, recover=False):
-        context.update(self.get_extra_context(request))
+        context.update(self.get_extra_context(request))  # FIXME
         return super(ItemEditor, self).render_revision_form(
             request, obj, version, context, revert, recover)
