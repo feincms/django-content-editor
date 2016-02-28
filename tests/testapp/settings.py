e7d2772a65da3634614986cab53fb18bbdcb9db2
Matthias Kestenholz <mk@feinheit.ch>
2016-02-28 13:18:41 +0100

    One less warning

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -56,5 +56,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.locale.LocaleMiddleware'
+    'django.middleware.locale.LocaleMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 )
