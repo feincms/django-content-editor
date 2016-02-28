80adc37fca978bd815f3f4f338863266fca84611
Matthias Kestenholz <mk@feinheit.ch>
2016-02-28 15:18:01 +0100

    Add PluginRenderer... without tests and/or docs yet

---
+++ content_editor/renderer.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, unicode_literals
+
+from collections import defaultdict
+
+from django.utils.html import format_html_join, mark_safe
+
+
+class PluginRenderer(object):
+    def __init__(self):
+        def default_renderer(plugin):
+            return mark_safe('<!-- %s: %s -->' % (
+                plugin._meta.label,
+                plugin,
+            ))
+
+        self._renderers = defaultdict(lambda: default_renderer)
+
+    def register(self, plugin, renderer):
+        self._renderers[plugin] = renderer
+
+    def render(self, plugins):
+        return format_html_join('\n', '{}', (
+            (self._renderers[plugin.__class__](plugin),)
+            for plugin in plugins
+        ))
+
+    def plugins(self):
+        return self._renderers.keys()
