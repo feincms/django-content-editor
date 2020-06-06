c008171b93371c72a2f2a2698f514d267e312837
Matthias Kestenholz <mk@feinheit.ch>
2020-06-06 18:02:32 +0200

    Switch from url() to re_path()

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 
 from .views import ArticleView, PageView
 
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"^articles/(?P<pk>\d+)/$", ArticleView.as_view(), name="article_detail"),
-    url(r"^pages/(?P<pk>\d+)/$", PageView.as_view(), name="page_detail"),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^articles/(?P<pk>\d+)/$", ArticleView.as_view(), name="article_detail"),
+    re_path(r"^pages/(?P<pk>\d+)/$", PageView.as_view(), name="page_detail"),
 ]
