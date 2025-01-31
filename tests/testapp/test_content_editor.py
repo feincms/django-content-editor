0e89101601e8b22edb074dacb4bf9efd0db5be0f
Matthias Kestenholz <mk@feinheit.ch>
2025-01-31 09:22:20 +0100

    Convert the tests to pytest

--- tests/testapp/test_content_editor.py
+++ tests/testapp/test_content_editor.py
@@ -1,11 +1,12 @@
+import pytest
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.core import checks
 from django.core.exceptions import ImproperlyConfigured
-from django.db import models
-from django.test import TestCase
-from django.test.utils import isolate_apps
+from django.db import connection, models
+from django.test.utils import CaptureQueriesContext, isolate_apps
 from django.urls import reverse
+from pytest_django.asserts import assertContains
 
 from content_editor.admin import ContentEditor, ContentEditorInline
 from content_editor.contents import Contents, contents_for_item
@@ -13,244 +14,252 @@ from content_editor.models import Region
 from testapp.models import Article, Download, Page, PageText, RichText
 
 
-class ContentEditorTest(TestCase):
-    def login(self):
-        u = User(username="test", is_active=True, is_staff=True, is_superuser=True)
-        u.set_password("test")
-        u.save()
-        self.assertTrue(self.client.login(username="test", password="test"))
+@pytest.fixture
+def user():
+    u = User.objects.create(
+        username="test", is_active=True, is_staff=True, is_superuser=True
+    )
+    u.set_password("test")
+    u.save()
+    return u
 
-    def test_stuff(self):
-        # Smoke test some stuff
-        article = Article.objects.create(title="Test")
-        richtext = article.testapp_richtext_set.create(
-            text="<p>bla</p>", region="main", ordering=10
-        )
-        article.testapp_download_set.create(file="bla.pdf", region="main", ordering=20)
 
-        with self.assertNumQueries(2):  # Two content types.
-            contents = contents_for_item(article, plugins=[RichText, Download])
+@pytest.fixture
+def client(client, user):
+    client.login(username="test", password="test")
+    return client
 
-            self.assertEqual(contents.main[0], richtext)
-            self.assertEqual(contents.main[0].parent, article)
 
-        self.assertEqual(len(contents.main), 2)
-        self.assertEqual(len(contents.sidebar), 0)
-        self.assertEqual(len(contents.bla), 0)  # No AttributeError
+@pytest.mark.django_db
+def test_stuff(client):
+    article = Article.objects.create(title="Test")
+    richtext = article.testapp_richtext_set.create(
+        text="<p>bla</p>", region="main", ordering=10
+    )
+    article.testapp_download_set.create(file="bla.pdf", region="main", ordering=20)
 
-        response = self.client.get(article.get_absolute_url())
-        self.assertContains(response, "<h1>Test</h1>")
-        self.assertContains(response, "<p>bla</p>")
-
-        # Test for Contents.__iter__
+    with CaptureQueriesContext(connection) as ctx:
         contents = contents_for_item(article, plugins=[RichText, Download])
-        self.assertFalse(contents._sorted)
-        self.assertEqual(len(list(contents)), 2)
-        self.assertTrue(contents._sorted)
-        self.assertEqual(len(list(contents)), 2)
+        assert len(ctx.captured_queries) == 2  # Two content types
 
-        # Contents.__len__ also means that a Contents instance may be falsy
-        self.assertEqual(len(contents), 2)
-        self.assertTrue(contents)
-        self.assertFalse(contents_for_item(article, []))
+        assert contents.main[0] == richtext
+        assert contents.main[0].parent == article
 
-        # Regions may be limited
-        contents = contents_for_item(
-            article,
-            plugins=[RichText, Download],
-            regions=[Region(key="other", title="other")],
-        )
-        self.assertEqual(list(contents), [])
+    assert len(contents.main) == 2
+    assert len(contents.sidebar) == 0
+    assert len(contents.bla) == 0  # No AttributeError
 
-    def test_admin(self):
-        self.login()
-        response = self.client.get(reverse("admin:testapp_article_add"))
+    response = client.get(article.get_absolute_url())
+    assertContains(response, "<h1>Test</h1>")
+    assertContains(response, "<p>bla</p>")
 
-        self.assertContains(response, '_editor.js" data-context="{&quot;', 1)
-        self.assertContains(response, 'id="content-editor-context"></sc', 1)
-        self.assertContains(response, 'class="richtext"', 1)
-        self.assertContains(
-            response,
-            "&quot;prefix&quot;: &quot;testapp_richtext_set&quot;",
-            # 2,  (Once by us, and once by inlines.js)
-        )
+    # Test for Contents.__iter__
+    contents = contents_for_item(article, plugins=[RichText, Download])
+    assert not contents._sorted
+    assert len(list(contents)) == 2
+    assert contents._sorted
+    assert len(list(contents)) == 2
 
-        article = Article.objects.create(title="Test")
+    # Contents.__len__ also means that a Contents instance may be falsy
+    assert len(contents) == 2
+    assert contents
+    assert not contents_for_item(article, [])
 
-        response = self.client.get(
-            reverse("admin:testapp_article_change", args=(article.pk,))
-        )
-        self.assertContains(response, 'value="Test"', 1)
+    # Regions may be limited
+    contents = contents_for_item(
+        article,
+        plugins=[RichText, Download],
+        regions=[Region(key="other", title="other")],
+    )
+    assert list(contents) == []
 
-    def test_empty(self):
-        article = Article.objects.create(title="Test")
 
-        with self.assertNumQueries(2):
-            contents = contents_for_item(article, plugins=[RichText, Download])
+@pytest.mark.django_db
+def test_admin(client):
+    response = client.get(reverse("admin:testapp_article_add"))
 
-        self.assertEqual(contents.main, [])
+    assertContains(response, '_editor.js" data-context="{&quot;', 1)
+    assertContains(response, 'id="content-editor-context"></sc', 1)
+    assertContains(response, 'class="richtext"', 1)
+    assertContains(
+        response,
+        "&quot;prefix&quot;: &quot;testapp_richtext_set&quot;",
+    )
 
-        with self.assertNumQueries(0):
-            contents = contents_for_item(article, plugins=[])
+    article = Article.objects.create(title="Test")
 
-        self.assertEqual(contents.main, [])
+    response = client.get(reverse("admin:testapp_article_change", args=(article.pk,)))
+    assertContains(response, 'value="Test"', 1)
 
-    def test_unknown_regions(self):
-        article = Article.objects.create(title="Test")
-        for idx, region in enumerate(("", "notexists", "main")):
-            RichText.objects.create(
-                parent=article, ordering=idx, region=region, text="Test"
-            )
 
-        contents = contents_for_item(article, plugins=[RichText])
-        self.assertEqual(len(contents._unknown_region_contents), 2)
+@pytest.mark.django_db
+def test_empty():
+    article = Article.objects.create(title="Test")
 
-    def test_inheritance(self):
-        page = Page.objects.create(title="root")
-        child = page.children.create(title="child 1")
+    with CaptureQueriesContext(connection) as ctx:
+        contents = contents_for_item(article, plugins=[RichText, Download])
+        assert len(ctx.captured_queries) == 2
 
-        with self.assertNumQueries(1):
-            contents = contents_for_item(child, plugins=[PageText], inherit_from=[page])
-            self.assertEqual(contents.main, [])
-            self.assertEqual(contents.sidebar, [])
+    assert contents.main == []
 
-        page.testapp_pagetext_set.create(
-            region="main", ordering=10, text="page main text"
-        )
-        page.testapp_pagetext_set.create(
-            region="sidebar", ordering=20, text="page sidebar text"
-        )
+    with CaptureQueriesContext(connection) as ctx:
+        contents = contents_for_item(article, plugins=[])
+        assert len(ctx.captured_queries) == 0
 
-        with self.assertNumQueries(1):
-            contents = contents_for_item(child, plugins=[PageText], inherit_from=[page])
-            self.assertEqual(contents.main, [])
-            self.assertEqual([c.text for c in contents.sidebar], ["page sidebar text"])
+    assert contents.main == []
 
-            self.assertEqual(contents.sidebar[0].parent, page)
 
-        child.testapp_pagetext_set.create(
-            region="sidebar", ordering=10, text="child sidebar text"
+@pytest.mark.django_db
+def test_unknown_regions():
+    article = Article.objects.create(title="Test")
+    for idx, region in enumerate(("", "notexists", "main")):
+        RichText.objects.create(
+            parent=article, ordering=idx, region=region, text="Test"
         )
 
-        child.testapp_pagetext_set.create(
-            region="main", ordering=20, text="child main text"
+    contents = contents_for_item(article, plugins=[RichText])
+    assert len(contents._unknown_region_contents) == 2
+
+
+@pytest.mark.django_db
+def test_inheritance(client):
+    page = Page.objects.create(title="root")
+    child = page.children.create(title="child 1")
+
+    with CaptureQueriesContext(connection) as ctx:
+        contents = contents_for_item(child, plugins=[PageText], inherit_from=[page])
+        assert len(ctx.captured_queries) == 1
+        assert contents.main == []
+        assert contents.sidebar == []
+
+    page.testapp_pagetext_set.create(region="main", ordering=10, text="page main text")
+    page.testapp_pagetext_set.create(
+        region="sidebar", ordering=20, text="page sidebar text"
+    )
+
+    with CaptureQueriesContext(connection) as ctx:
+        contents = contents_for_item(child, plugins=[PageText], inherit_from=[page])
+        assert len(ctx.captured_queries) == 1
+        assert contents.main == []
+        assert [c.text for c in contents.sidebar] == ["page sidebar text"]
+        assert contents.sidebar[0].parent == page
+
+    child.testapp_pagetext_set.create(
+        region="sidebar", ordering=10, text="child sidebar text"
+    )
+    child.testapp_pagetext_set.create(
+        region="main", ordering=20, text="child main text"
+    )
+
+    with CaptureQueriesContext(connection) as ctx:
+        contents = contents_for_item(child, plugins=[PageText], inherit_from=[page])
+        assert len(ctx.captured_queries) == 1
+        assert [c.text for c in contents.main] == ["child main text"]
+        assert [c.text for c in contents.sidebar] == ["child sidebar text"]
+        assert contents.sidebar[0].parent == child
+
+    response = client.get(child.get_absolute_url())
+    assertContains(response, "child main text")
+    assertContains(response, "child sidebar text")
+
+
+@isolate_apps()
+def test_model_checks():
+    class Model(models.Model):
+        name = models.CharField()
+
+        def __str__(self):
+            return self.name
+
+    class ModelAdmin(ContentEditor):
+        model = Model
+        inlines = []
+
+    assert ModelAdmin(Model, admin.AdminSite()).check() == [
+        checks.Error(
+            "ContentEditor models require a non-empty 'regions'"
+            " attribute or property.",
+            obj=ModelAdmin,
+            id="content_editor.E002",
         )
+    ]
 
-        with self.assertNumQueries(1):
-            contents = contents_for_item(child, plugins=[PageText], inherit_from=[page])
-            self.assertEqual([c.text for c in contents.main], ["child main text"])
-            self.assertEqual([c.text for c in contents.sidebar], ["child sidebar text"])
 
-            self.assertEqual(contents.sidebar[0].parent, child)
+def test_inline_checks():
+    assert admin.ModelAdmin(Article, admin.AdminSite()).check() == []
 
-        response = self.client.get(child.get_absolute_url())
-        self.assertContains(response, "child main text")
-        self.assertContains(response, "child sidebar text")
+    class RichTextInline(ContentEditorInline):
+        model = RichText
+        # Purposefully construct an inline with missing region
+        # and ordering fields
+        fieldsets = [(None, {"fields": ("text",)})]
 
-    @isolate_apps()
-    def test_model_checks(self):
-        class Model(models.Model):
-            name = models.CharField()
+    class InvalidRegionsStringInline(ContentEditorInline):
+        model = RichText
+        regions = "main"
 
-            def __str__(self):
-                return self.name
+    class InvalidRegionsCallableInline(ContentEditorInline):
+        model = RichText
 
-        class ModelAdmin(ContentEditor):
-            model = Model
-            inlines = []
+        def regions(self, all_regions):
+            return "main"
 
-        self.assertEqual(
-            ModelAdmin(Model, admin.AdminSite()).check(),
-            [
-                checks.Error(
-                    "ContentEditor models require a non-empty 'regions'"
-                    " attribute or property.",
-                    obj=ModelAdmin,
-                    id="content_editor.E002",
-                )
-            ],
-        )
+    class ValidRegionsGeneratorInline(ContentEditorInline):
+        model = RichText
 
-    def test_inline_checks(self):
-        self.assertEqual(
-            admin.ModelAdmin(Article, admin.AdminSite()).check(),
-            [],
-        )
+        def regions(self, all_regions):
+            yield "main"
 
-        class RichTextInline(ContentEditorInline):
-            model = RichText
-            # Purposefully construct an inline with missing region
-            # and ordering fields
-            fieldsets = [(None, {"fields": ("text",)})]
+    class ArticleAdmin(ContentEditor):
+        model = Article
+        inlines = [
+            RichTextInline,
+            InvalidRegionsStringInline,
+            InvalidRegionsCallableInline,
+            ValidRegionsGeneratorInline,
+        ]
 
-        class InvalidRegionsStringInline(ContentEditorInline):
-            model = RichText
-            regions = "main"
+    assert ArticleAdmin(Article, admin.AdminSite()).check() == [
+        checks.Error(
+            "fieldsets must contain both 'region' and 'ordering'.",
+            obj=RichTextInline,
+            id="content_editor.E001",
+        ),
+        checks.Error(
+            "regions must be 'None' or an iterable. Current value is 'main'.",
+            obj=InvalidRegionsStringInline,
+            id="content_editor.E003",
+        ),
+        checks.Error(
+            "regions must be 'None' or an iterable. Current value is 'main'.",
+            obj=InvalidRegionsCallableInline,
+            id="content_editor.E003",
+        ),
+    ]
 
-        class InvalidRegionsCallableInline(ContentEditorInline):
-            model = RichText
 
-            def regions(self, all_regions):
-                return "main"
+def test_invalid_region_objects():
+    with pytest.raises(ImproperlyConfigured):
+        Region(key="regions", title="regions")
+    with pytest.raises(ImproperlyConfigured):
+        Region(key="_private", title="private")
+    with pytest.raises(ImproperlyConfigured):
+        Region(key="content-main", title="content")
+    with pytest.raises(TypeError):
+        Region(key="valid")  # title is missing
 
-        class ValidRegionsGeneratorInline(ContentEditorInline):
-            model = RichText
 
-            def regions(self, all_regions):
-                yield "main"
+def test_invalid_content_regions():
+    c = Contents([Region(key="main", title="main")])
 
-        class ArticleAdmin(ContentEditor):
-            model = Article
-            inlines = [
-                RichTextInline,
-                InvalidRegionsStringInline,
-                InvalidRegionsCallableInline,
-                ValidRegionsGeneratorInline,
-            ]
+    assert c.blub == []
+    assert c["blub"] == []
 
-        self.assertEqual(
-            ArticleAdmin(Article, admin.AdminSite()).check(),
-            [
-                checks.Error(
-                    "fieldsets must contain both 'region' and 'ordering'.",
-                    obj=RichTextInline,
-                    id="content_editor.E001",
-                ),
-                checks.Error(
-                    "regions must be 'None' or an iterable. Current value is 'main'.",
-                    obj=InvalidRegionsStringInline,
-                    id="content_editor.E003",
-                ),
-                checks.Error(
-                    "regions must be 'None' or an iterable. Current value is 'main'.",
-                    obj=InvalidRegionsCallableInline,
-                    id="content_editor.E003",
-                ),
-            ],
-        )
+    with pytest.raises(AttributeError):
+        _read = c._blub
+    with pytest.raises(KeyError):
+        c["_blub"]
 
-    def test_invalid_region_objects(self):
-        with self.assertRaises(ImproperlyConfigured):
-            Region(key="regions", title="regions")
-        with self.assertRaises(ImproperlyConfigured):
-            Region(key="_private", title="private")
-        with self.assertRaises(ImproperlyConfigured):
-            Region(key="content-main", title="content")
-        with self.assertRaises(TypeError):
-            Region(key="valid")  # title is missing
 
-    def test_invalid_content_regions(self):
-        c = Contents([Region(key="main", title="main")])
-
-        self.assertEqual(c.blub, [])
-        self.assertEqual(c["blub"], [])
-
-        with self.assertRaises(AttributeError):
-            _read = c._blub
-        with self.assertRaises(KeyError):
-            c["_blub"]
-
-    def test_hashable_types(self):
-        _mapping = {
-            Region(key="hello", title="Hello"): Region(key="world", title="World")
-        }
+def test_hashable_types():
+    _mapping = {Region(key="hello", title="Hello"): Region(key="world", title="World")}
